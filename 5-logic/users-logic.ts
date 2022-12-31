4c7395434dca377b1676c56e177fc1bfc02301d9
harelsarag7 <112413505+harelsarag7@users.noreply.github.com>
2023-01-01 00:32:53 +0200

    changed to db

--- 5-logic/users-logic.ts
+++ 5-logic/users-logic.ts
@@ -1,30 +1,49 @@
-import { dal } from "../2-utils/dal";
+// import { dal } from "../2-utils/dal";
+import { OkPacket } from "mysql2";
+import { execute } from "../2-utils/dal";
+import { CardModel } from "../4-models/CardModel";
 import { UserModel, UserRole } from "../4-models/UserModel";
 
-export function getUsers() {
-    return dal.getAllUsers();
+// export function getUsers() {
+//     // return dal.getAllUsers();
+// }
+
+export async function getUsers(): Promise<UserModel[]>{
+    const query = "SELECT * FROM businesscard.users";
+    const [rows] = await execute<UserModel[]>(query);
+    return rows
 }
 
-export async function getUser(id: number) {
-    // Get all users
-    const users = await getUsers();
-
-    // Find specific book
-    const user = users.find(b => b.id === id);
-    if (!user) throw new Error();
-
-    return user;
+export async function getUser(id: number): Promise<UserModel>{
+    const query = "SELECT * FROM businesscard.users WHERE id = ?";
+    const [rows] = await execute<UserModel[]>(query, [id]);
+    if(rows.length === 0) return null;
+    return rows[0];
 }
+// export async function getUser(id: number) {
+//     // Get all users
+//     const users = await getUsers();
 
-export async function addUser(user: UserModel) {
-    const users = await getUsers();
+//     // Find specific book
+//     // const user = users.find(b => b.id === id);
+//     // if (!user) throw new Error();
 
-    const id = Math.max(...users.map(b => b.id)) + 1;
-    user.id = id;
-    user.role = UserRole.User;
-    users.push(user);
+//     // return user;
+// }
 
-    await dal.saveAllUsers(users);
+export async function addUser(firstName: string, lastName: string, email: string, phone: string, username: string, password: string) {
+    const query = `INSERT INTO businesscard.users(firstName, lastName, email, phone, username, password) VALUES('${firstName}', '${lastName}', '${email}', '${phone}', '${username}', '${password}')`;
+    
+    const [results] = await execute<OkPacket>(query)
 
-    return user;
+    const id = results.insertId;
+    return {
+        id,
+        firstName,
+        lastName,
+        email,
+        phone,
+        username,
+        password
+    };
 }
